7245d3ae47acba1be52f799147476bb693c74c36
Cody B. Daig <cody@daig.me>
2015-09-21 14:09:26 -0700

    [feat] is unique

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -7,13 +7,28 @@ module.exports = function (db) {
     return db.query('SELECT * FROM Users');
   };
 
+  Users.phoneIsUnique = function (phoneNumber) {
+    return db.query('SELECT COUNT(PhoneNumber) FROM Users WHERE PhoneNumber=\'' + phoneNumber + '\'')
+      .then(function (data) {
+        return data[0]['COUNT(PhoneNumber)'];
+      });
+  };
+
   Users.signupPhoneNumber = function (user) {
-    return db.query('INSERT INTO TempPhoneNumber (PhoneNumber, Code) VALUES (' + user.PhoneNumber + ', ' + user.code + ')')
-      .catch(function (err) {
-        if (err.code !== 'ER_DUP_ENTRY') {
-          throw err;
+    return Users.phoneIsUnique(user.PhoneNumber)
+      .then(function (count) {
+        if(count > 0) {
+          throw {code: 'ER_DUP_ENTRY'};
+        } else {
+          return db.query('INSERT INTO TempPhoneNumber (PhoneNumber, Code) VALUES (' + user.PhoneNumber + ', ' + user.code + ')')
+            .catch(function (err) {
+              if (err.code !== 'ER_DUP_ENTRY') {
+                throw err;
+              }
+            });
         }
       });
+
   };
 
   db.Users = Users;
