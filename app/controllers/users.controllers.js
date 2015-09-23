13cdbaa61a72d87354c72c4a7b8caff384496ab0
Cody B. Daig <cody@daig.me>
2015-09-23 11:08:43 -0700

    [feat] Twilio

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -1,10 +1,22 @@
 'use strict';
 
 var db = require('../../lib/db');
+var twilio = require('../../lib/twilio');
 
 exports.signupPhoneNumber = signupPhoneNumber;
+exports.signupVerifyCode = signupVerifyCode;
 
 //////////
+
+function catchError (req, res) {
+  return function (err) {
+    if(err.code === 'ER_DUP_ENTRY') {
+      res.status(409).send(err);
+    } else {
+      res.status(500).send(err);
+    }
+  }
+}
 
 function send200 (req, res) {
   return function (data) {
@@ -12,25 +24,40 @@ function send200 (req, res) {
   }
 }
 
-function send500 (req, res) {
-  return function (err) {
-    res.status(500).send(err);
-  }
-}
-
 function signupPhoneNumber (req, res) {
   var user = {
-    code: '1234',
+    code: Math.floor(Math.random() * 10000),
     PhoneNumber: req.body.PhoneNumber
   };
+
   db.Users.signupPhoneNumber(user)
     .then(function (data) {
       if (data) {
-        // send code
-        // doconsole.log('sending code: ', user.code);
+        return twilio.sendCode(user)
+          .then(function () {
+            return 'Success';
+          });
       }
       return 'Success';
     })
     .then(send200(req, res))
-    .catch(send500(req, res));
+    .catch(catchError(req, res));
+}
+
+function signupVerifyCode (req, res) {
+  var user = {
+    code: req.body.code,
+    PhoneNumber: req.body.PhoneNumber
+  };
+
+  db.Users.verifyPhoneNumber(user)
+    .then(function (data) {
+      if(data === true) {
+        return 'Success';
+      } else {
+        return 'Not Valid';
+      }
+    })
+    .then(send200(req, res))
+    .catch(catchError(req, res));
 }
